4864bcd6e11d70bc2972016004211711a86d5640
Samamtro <fofanasanoussy545@gmail.com>
2025-02-17 19:01:48 +0000

    file zat

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Taille de char: %lu octet(s)\n", sizeof(char));
+    printf("Taille de int: %lu octet(s)\n", sizeof(int));
+    printf("Taille de long int: %lu octet(s)\n", sizeof(long int));
+    printf("Taille de long long int: %lu octet(s)\n", sizeof(long long int));
+    printf("Taille de float: %lu octet(s)\n", sizeof(float));
+    return 0;
+}
