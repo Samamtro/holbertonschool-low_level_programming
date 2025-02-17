37da0dc58f6edc70d229456a182cdfc4822b2a9a
Samamtro <fofanasanoussy545@gmail.com>
2025-02-17 20:20:36 +0000

    file vut

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,14 @@
  */
 int main(void)
 {
-    printf("Taille de char: %lu octet(s)\n", sizeof(char));
-    printf("Taille de int: %lu octet(s)\n", sizeof(int));
-    printf("Taille de long int: %lu octet(s)\n", sizeof(long int));
-    printf("Taille de long long int: %lu octet(s)\n", sizeof(long long int));
-    printf("Taille de float: %lu octet(s)\n", sizeof(float));
-    return (0);
+printf("Taille des types de base en octets :\\n");
+printf("char: %lu octets\\n", sizeof(char));
+printf("short: %lu octets\\n", sizeof(short));
+printf("int: %lu octets\\n", sizeof(int));
+printf("long: %lu octets\\n", sizeof(long));
+printf("long long: %lu octets\\n", sizeof(long long));
+printf("float: %lu octets\\n", sizeof(float));
+printf("double: %lu octets\\n", sizeof(double));
+printf("long double: %lu octets\\n", sizeof(long double));
+return (0);
 }
