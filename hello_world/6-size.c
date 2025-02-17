f6a885f9b7553eaedd401048fa3ecebdbd2b4847
Samamtro <fofanasanoussy545@gmail.com>
2025-02-17 20:14:46 +0000

    file vut

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
     printf("Taille de char: %lu octet(s)\n", sizeof(char));
@@ -7,5 +11,5 @@ int main(void)
     printf("Taille de long int: %lu octet(s)\n", sizeof(long int));
     printf("Taille de long long int: %lu octet(s)\n", sizeof(long long int));
     printf("Taille de float: %lu octet(s)\n", sizeof(float));
-    return 0;
+    return (0);
 }
