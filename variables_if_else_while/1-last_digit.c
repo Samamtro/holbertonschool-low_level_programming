051160dd499d4d7fe0ac9a839a9b9ea299fc1c1b
Samamtro <fofanasanoussy545@gmail.com>
2025-02-18 14:36:13 +0000

    file hu

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -12,9 +12,9 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (last_digit  > 5)
+	if (last digit of n > 5)
 		printf("and is greater than 5\n");
-	else if (last_digit == 0)
+	else if (last digit of n == 0)
 		printf("and is 0\n");
 	else
 		printf("and is less than 6 and not 0\n");
