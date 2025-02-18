627c2a702c62e4ea0d2e83f49f2ee16a8cf4b06e
Samamtro <fofanasanoussy545@gmail.com>
2025-02-18 13:05:15 +0000

    file voi

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+
+putchar('4' + 2); // Affiche '6'
+putchar('\n');    // Nouvelle ligne
+return (0)
+
+}
