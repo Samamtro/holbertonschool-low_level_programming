00c721b091fd1379e9d21a22ea7a4f63a722687b
Samamtro <fofanasanoussy545@gmail.com>
2025-02-19 14:16:10 +0000

    file der

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    putchar('4' + 2); // Affiche '6'
-    putchar('\n');    // Nouvelle ligne
-    return 0;
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
 }
