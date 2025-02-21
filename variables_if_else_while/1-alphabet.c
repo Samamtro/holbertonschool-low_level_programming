39e035be3f88a00c255accf7d451792bbf9c6d33
Samamtro <fofanasanoussy545@gmail.com>
2025-02-21 08:24:47 +0000

    alphabet

--- variables_if_else_while/1-alphabet.c
+++ variables_if_else_while/1-alphabet.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet in lowercase followed by a new line
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
