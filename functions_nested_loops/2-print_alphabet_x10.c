d56cd585fc34e1e2f79b35985fdbfacf7d98cc50
Samamtro <fofanasanoussy545@gmail.com>
2025-02-21 09:03:19 +0000

    file tres

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
-	char a;
+	int count = 0;
+	char letter;
 
-	for (i = 0; i < 10; i++)
-
-		for (a = 'a'; a <= 'z'; a++)
-		{
-		_putchar(a);
-		}
-	 _putchar('\n');
+	while (count++ <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('\n');
+	}
 }
