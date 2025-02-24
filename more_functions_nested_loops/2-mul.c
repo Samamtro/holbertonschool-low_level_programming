0f811ccd8076c7df9a8dccaf7c6fed4586757502
Samatro <fofanasanoussy545@gmail.com>
2025-02-24 14:15:22 +0000

    file 2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul -multipling to 2 number
+ * @a: integer param1
+ * @b: integer param2
+ *
+ * Return: success to param
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
