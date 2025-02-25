6c3bbc0eee6979a8fd9bd8433535b72cb5cea614
Samatro <fofanasanoussy545@gmail.com>
2025-02-25 13:48:18 +0000

    file blood

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,10 @@ int main(void)
 	{
 		if (x % 15 == 0)
 			printf("FizzBuzz ");
-		else if (x % 5 == 0)
+		else if (x % 5 == 0 && x != 100)
 			printf("Buzz ");
+		else if (x % 5 == 0 && x == 100)
+			printf("Buzz");
 		else if (x % 3 == 0)
 			printf("Fizz ");
 		else printf("%d ", x);
