a2bcd45f6e53d926c8b01a7f241158ce69051b1c
Samamtro <fofanasanoussy545@gmail.com>
2025-03-02 14:00:31 +0100

    file tei

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - a function that concatenates two strings
+ * @src: second paramer
+ * Return: a string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
+}
+
