e7cdff346d307f189b1c7f4f408e42dc17b30484
Samamtro <fofanasanoussy545@gmail.com>
2025-03-03 10:10:43 +0100

    file grte

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - the function that fills memory with a constant byte.
+ * @s: startig the address of memory to print
+ * @b: the desired to value
+ * @n: the number to byte of change
+ *
+ * Return: a pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
