49f7ced044e95c3cfa7df03f3e0c04197823a3ad
Samamtro <fofanasanoussy545@gmail.com>
2025-03-05 12:29:50 +0100

    file bnc

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -17,5 +17,5 @@ void print_diagsums(int *a, int size)
 		sum1 += a[i * (size + 1)];
 		sum2 += a[(i * size) + (size - 1 - i)];
 	}
-	printf("%d, %d,'\n'", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
