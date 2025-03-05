305e8bf7c596c1aeecdc7d31946d303e5b51cc00
Samamtro <fofanasanoussy545@gmail.com>
2025-03-05 13:33:20 +0100

    file cam

---
+++ pointers_arrays_strings/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line
+ * @s: the string to print
+ *
+ * Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+}
+return;
