b76e54403cbd7c5c689645986c7b93b4801b9a40
Samamtro <fofanasanoussy545@gmail.com>
2025-03-07 00:15:49 +0100

    file mbs

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include "stdio.h"
 /**
  * main - Write a program that prints the number of arguments passed into it
- * @args: args This is the argument count
- * @argv: argv This is the argument vector
+ * @args: This is the argument count (includes the program name)
+ * @argv: This is the argument vector (array of strings)
  *
- * Return: success always
+ * Return: success always 0
  */
 int main(int args, char *argv[])
 {
 	printf("%d\n", args - 1);
-	(void)argv; 
+	(void)argv;
 	return (0);
 }
