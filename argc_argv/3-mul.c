91382316a722a0c4416221347aaadbfc106ce9bc
Samamtro <fofanasanoussy545@gmail.com>
2025-03-07 11:38:58 +0100

    file blee

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplie  two argument to result
+ * @argc: this is the number to argument
+ * @argv: This is the The table containing the argumen
+ * @result: this have to multiple
+ *
+ * Return: always error, sucess 1
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
+}
+
