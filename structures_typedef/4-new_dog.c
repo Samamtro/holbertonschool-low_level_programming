49f32e00bfae7cb8ab405d2b1f510ff706abbd50
Samamtro <fofanasanoussy545@gmail.com>
2025-03-14 20:21:15 +0100

    file ghrt

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,35 +15,46 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newDog;
 	int lName, lOwner, i;
 
+/* Allocation de mémoire pour la structure dog_t */
 	newDog = malloc(sizeof(struct dog));
 	if (newDog == NULL)
 		return (NULL);
 
+	/* Calcul de la longueur de name (y compris le caractère nul '\0') */
 	for (lName = 0; *(name + lName); lName++)
 		;
 	lName++;
+	/* Calcul de la longueur de owner (y compris le caractère nul '\0') */
 	for (lOwner = 0; *(owner + lOwner); lOwner++)
 		;
 	lOwner++;
 
+	/* Allocation de mémoire pour le nom du chien */
 	newDog->name = malloc(lName * sizeof(char));
 	if (newDog->name == NULL)
 	{
+		/*Libération de la structure si l'allocation échoue */
 		free(newDog);
 		return (NULL);
 	}
+	/* Copie du nom dans la mémoire nouvellement allouée */
 	for (i = 0; i < lName; i++)
 		*(newDog->name + i) = *(name + i);
 
+	/* Assignation de l'âge du chien */
 	newDog->age = age;
 
+	/* Allocation de mémoire pour le propriétaire du chien */
 	newDog->owner = malloc(lOwner * sizeof(char));
 	if (newDog->owner == NULL)
+
 	{
+		/* Libération de la mémoire allouée précédemment */
 		free(newDog->name);
 		free(newDog);
 		return (NULL);
 	}
+	/* Copie du propriétaire dans la mémoire nouvellement allouée */
 	for (i = 0; i < lOwner; i++)
 	*(newDog->owner + i) = *(owner + i);
 
