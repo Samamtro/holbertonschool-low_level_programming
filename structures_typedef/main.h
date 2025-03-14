768cf08641c7deecbb8e94a96ef2018edb37c09e
Samamtro <fofanasanoussy545@gmail.com>
2025-03-14 14:02:18 +0100

    file gratte

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include "dog.h"
 int _putchar(char c);
 #endif /* MAIN_H */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
-
