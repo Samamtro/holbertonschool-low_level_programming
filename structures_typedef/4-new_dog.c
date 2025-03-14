b6751e36fc0de90df8f1d65951ae31094a468e4d
Samamtro <fofanasanoussy545@gmail.com>
2025-03-14 13:12:19 +0100

    file couter

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include "dog.h"	/* Inclut le fichier d'en-tête qui contient la définition
+* de la structure `dog_t`
+*/
+#include <stdlib.h>	/* Inclut la bibliothèque standard qui permet d'utiliser
+* malloc et free
+*/
+
+/**
+ * new_dog - entry pointer
+ * new_dog - a function that creates a new dog
+ * @name: the string from main, name to the pret
+ * @age: the string from main, age to the pret
+ * @owner: the string from main, owner to the pret
+ *
+ * Return: p
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	p = malloc(sizeof(dog_t));	/* Alloue de la mémoire pour un objet de type
+	* `dog_t`
+	*/
+	if (p == NULL)
+	{
+		return (NULL);	/* Si l'allocation échoue, retourne NULL */
+	}
+	if (name == NULL)
+	{
+		free(p);	/* Libère la mémoire allouée pour `p` si `name` est NULL */
+		free(owner);	/* Libère la mémoire allouée pour `owner` si `owner`
+		* est NULL
+		*/
+		return (NULL);	/* Retourne NULL pour signaler l'erreur */
+	}
+	if (owner == NULL)
+	{
+		free(p);	/* Libère la mémoire allouée pour `p` si `owner` est NULL */
+		free(name);	/* Libère la mémoire allouée pour `name` si `name` est NULL
+		*/
+		return (NULL);	/* Retourne NULL pour signaler l'erreur */
+	}
+	p->name = name;	/* Initialise le champ `name` de la structure `dog_t` */
+	p->owner = owner;	/* Initialise le champ `owner` de la structure `dog_t`
+	*/
+	p->age = age;	/* Initialise le champ `age` de la structure `dog_t` */
+	return (p);	/* Retourne le pointeur vers l'objet `dog_t` nouvellement créé.
+	*/
+}
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -3,4 +3,5 @@
 int _putchar(char c);
 #endif /* MAIN_H */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
