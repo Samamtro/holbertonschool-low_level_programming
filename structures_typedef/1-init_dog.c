ae3c908c473196e3735afdba4bc9cfb1e61bc393
Samamtro <fofanasanoussy545@gmail.com>
2025-03-14 11:23:10 +0100

    file ket

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,10 +11,15 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	/* Vérification si le pointeur d est NULL pour éviter un accès à une adresse
+	* invalide
+	*/
 	if (d == NULL)
+	/* Instruction vide (inutile ici, pourrait être remplacée par un return) */
 	;
 	else
 	{
+		/* Affectation des valeurs aux membres de la structure */
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
