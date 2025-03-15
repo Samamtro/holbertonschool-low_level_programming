738c1bfcab2253a166c5587e2252254b885aee0c
Samamtro <fofanasanoussy545@gmail.com>
2025-03-15 23:25:46 +0100

    file nest

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -5,9 +5,13 @@
  */
 void print_alphabet(void)
 {
+	/* Déclaration d'une variable de type char pour stocker chaque lettre*/
 	char a;
 
+	/* Boucle qui parcourt les lettres de 'a' à 'z'*/
 	for (a = 'a'; a <= 'z'; a++)
+	/* Affichage de la lettre actuelle */
 		_putchar(a);
+	/* Ajout d'un retour à la ligne après l'affichage de l'alphabet */
 	_putchar('\n');
 }
--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,20 +6,30 @@
 
 void print_alphabet_x10(void)
 {
+	/* Déclaration de la variable pour stocker les lettres de l'alphabet */
 	char ch;
+	/* Déclaration d'un compteur pour suivre le nombre de répétitions */
 	int i;
 
+	/* Initialisation du compteur à 0 */
 	i = 0;
 
+	/* Boucle pour répéter l'affichage de l'alphabet 10 fois */
 	while (i < 10)
 	{
+	/* Initialisation de la variable ch à la première lettre de l'alphabet */
 		ch = 'a';
+		/* Boucle pour parcourir l'alphabet de 'a' à 'z' */
 		while (ch <= 'z')
 		{
+		/* Affichage du caractère courant */
 			_putchar(ch);
+		/* Passage au caractère suivant */
 			ch++;
 		}
+		/* Passage à la ligne après l'affichage d'un alphabet complet */
 		_putchar('\n');
+		/* Incrémentation du compteur pour la prochaine itération */
 		i++;
 	}
 }
