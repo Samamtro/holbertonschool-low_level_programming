17eec3d427c0548db31915244affe829dfe07aa7
Samamtro <fofanasanoussy545@gmail.com>
2025-03-17 14:00:46 +0100

    file blo

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,7 +6,8 @@
  * @size: is the number of elements in the array
  * @cmp: is a pointer to the function to be used to compare values
  *
- * Return: 0 if false, something else otherwise
+ * Return: the index of the first element for which cmp does not return 0,
+ * or -1 if no such element is found or if size is less than or equal to 0
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
