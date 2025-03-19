af0a648277a85e7efa305b5ffd7857ea9d3976dd
Samamtro <fofanasanoussy545@gmail.com>
2025-03-19 10:59:04 +0100

    file difficile

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,9 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
+	int a, b, resultat;
 	int (*o)(int, int);
-	char op;
 
 	if (argc != 4)
 	{
@@ -20,30 +19,24 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	o = get_op_func(argv[2]);
+
 	if (o == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	op = argv[2][0];
-
-	if ((op == '/' || op == '%') && b == 0)
+	if ((*argv[2] == '/' || *argv[2] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+	resultat = o(a, b);
 
-	printf("%d\n", o(a, b));
+	printf("%d\n", resultat);
 
 	return (0);
 }
