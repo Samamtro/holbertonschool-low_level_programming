0941abb7bcb9a9db16abca34c3fb1e99fb200c0d
Samamtro <fofanasanoussy545@gmail.com>
2025-03-20 20:55:53 +0100

    file variable

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,6 +1,9 @@
 #include "variadic_functions.h"
+/*Inclusion du fichier d'en-tête contenant le prototype de la fonction*/
 #include <stdio.h>
+/* Inclusion de la bibliothèque standard pour utiliser printf */
 #include <stdarg.h>
+/* Inclusion de stdarg.h pour gérer les arguments variables */
 
 /**
  * print_strings - a function that prints strings, followed by a new line
@@ -12,25 +15,40 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
+	/* Déclaration d'une liste d'arguments variables */
 	char *str;
+	/* Pointeur pour stocker temporairement chaque chaîne de caractères */
 	unsigned int index;
+	/* Variable pour parcourir les arguments */
 
+/* Initialisation de la liste des arguments variables */
 	va_start(strings, n);
 
+	/* Boucle pour récupérer et afficher chaque chaîne passée en paramètre */
 	for (index = 0; index < n; index++)
 	{
+		/* Récupération de l'argument suivant dans la liste */
 		str = va_arg(strings, char *);
 
+		/* Vérifie si la chaîne est NULL */
 		if (str == NULL)
+		/* Affiche "(nil)" au lieu d'une valeur NULL */
 			printf("(nil)");
 		else
+		/* Affiche la chaîne normale */
 			printf("%s", str);
 
+		/*
+		 * Affiche le séparateur uniquement si ce n'est pas le dernier
+		 * élément et si le séparateur n'est pas NULL
+		 */
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 
+	/* Ajoute une nouvelle ligne après l'affichage des chaînes */
 	printf("\n");
 
+	/* Libère la mémoire utilisée par va_list */
 	va_end(strings);
 }
