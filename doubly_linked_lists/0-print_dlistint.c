02a002868bf286cd2ae9f4c4e6c76d20fd5ccb65
Samamtro <fofanasanoussy545@gmail.com>
2025-04-01 22:25:25 +0200

    file creates dlistint_len

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,24 +3,21 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_dlistint - prints all the elements of a dlistint_t list
+ * print_distint - prints all the elements of a dlistint_t list
  * @h: pointer to the list
  *
  * Return: the number of nodes
 */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t count = 0;	/* compteur de noeuds */
-	/* Parcours de la liste jusqu'à la fin */
+	size_t count = 0;	/
 
-	while (h != NULL)	/* tant que le pointeur n'est pas NULL */
-	/* on affiche la valeur du noeud courant */
-	/* et on passe au noeud suivant */
-	/* on incremente le compteur */
+	while (h)
 	{
-		printf("%d\n", h->n);	/* on affiche la valeur du noeud courant */
-		h = h->next;	/* on passe au noeud suivant */
-		count++;	/* on incremente le compteur */
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
 	}
-	return (count);	/* on retourne le nombre de noeuds */
+	return (count);
+
 }
---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - the number of elements in a linked dlistint_t list
+ * @h: Pointer to the head of the doubly linked list.
+ *
+ *	Return: The number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;	/* Initialise le compteur de nœuds à 0 */
+
+	while (h != NULL)	/* Parcourt la liste jusqu'à la fin (h devient NULL) */
+	{
+		count++;	/* Incrémente le compteur à chaque nœud */
+		h = h->next;/* on passe au noeud suivant */
+	}
+	return (count);	/* Retourne le nombre total de noeud */
+}
+
