a85efefc79323f2c8d50466b735508c9b0ab0f99
Samamtro <fofanasanoussy545@gmail.com>
2025-04-01 10:56:03 +0200

    file creates print list function

--- singly_linked_lists/main.c
+++ singly_linked_lists/main.c
@@ -1,7 +1,4 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include "lists.h"
 
 /**
  * main - check the code
@@ -10,31 +7,6 @@
  */
 int main(void)
 {
-    list_t *head;
-    list_t *new;
-    list_t hello = {"World", 5, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->str = strdup("Hello");
-    new->len = 5;
-    new->next = head;
-    head = new;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    printf("\n");
-    free(new->str);
-    new->str = NULL;
-    n = print_list(head);
-    printf("-> %lu elements\n", n);
-
-    free(new);
-    return (0);
+	printf("Total nodes: %lu\n", count);
+	return (0);
 }
