b6dc45022ae1486a2a6410bda7f3243f15e632f9
Samamtro <fofanasanoussy545@gmail.com>
2025-04-01 19:48:17 +0200

    create to print list free

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -10,15 +10,15 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp;
+	list_t *temp;	/* Pointeur temporaire pour stocker le nœud actuel */
 
-	while (head != NULL)
+	while (head != NULL)	/* Parcours de la liste jusqu'à la fin */
 	{
-		temp = head;
-		head = head->next;
-		free(temp->str);
-		free(temp);
-		temp = NULL;
+		temp = head;	/* stocke le noeud actuel */
+		head = head->next;	/* Déplace le pointeur vers le noeud suivant */
+		free(temp->str);	/* libère la chaine de caracteres */
+		free(temp);	/* libère le noeud actuel */
+		temp = NULL;	/* Réinitialise le pointeur temporaire */
 	}
 }
 
