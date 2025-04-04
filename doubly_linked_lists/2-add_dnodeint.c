4d3f39dc235a57dced6637956cc4c6167a7022cc
Samamtro <fofanasanoussy545@gmail.com>
2025-04-04 11:29:59 +0200

    file 2-add_dnodeint

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,22 +12,23 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;	/* Pointer to the new node */
+	dlistint_t *new_node;	/* Déclaration d'un pointeur pour le nouveau nœud */
 
-	/* Allocate memory for the new node */
+	/* Allocation de mémoire pour le nouveau nœud */
 	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)	/* Check if memory allocation was successful */
-		return (NULL);	/* Return NULL if it failed */
+	if (new_node == NULL)	/* Vérification de la réussite de l'allocation mémoire */
+		return (NULL);	/* Retourne NULL si l'allocation a échoué */
+	/* Initialisation des valeurs du nouveau nœud */
+	new_node->n = n;	/* Assigne la valeur n au nœud */
+	new_node->prev = NULL;	/* Le nouveau nœud est le premier, donc prev est NULL */
+	new_node->next = *head;	/* Son next pointe vers l'ancien premier élément */
 
-	new_node->n = n;	/* Set the value of the new node */
-	new_node->prev = NULL;	/* Set the previous pointer to NULL */
-	new_node->next = *head;	/* Set the next pointer to the current head */
+	/* Si la liste n'était pas vide, mettre à jour l'ancien premier élément */
+	if (*head != NULL)
+		(*head)->prev = new_node;	/* Met à jour le prev de l'ancien premier nœud */
 
-	if (*head != NULL)	/* If the list is not empty */
-	/* Set the previous pointer of the current head to the new node */
-		(*head)->prev = new_node;
 
-	*head = new_node;	/* Update the head to point to the new node */
+	*head = new_node;	 /* Mettre à jour le pointeur head pour pointer vers le nouveau nœud */
 
-	return (new_node);	/* Return the address of the new element */
+	return (new_node);	/* Retourne l'adresse du nouveau nœud inséré */
 }
