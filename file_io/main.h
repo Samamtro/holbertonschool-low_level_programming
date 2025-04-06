0a917f54bcd1b7332bb5529a1b75018a17fe48b3
Samamtro <fofanasanoussy545@gmail.com>
2025-04-06 17:05:19 +0200

    file cp

--- file_io/main.h
+++ file_io/main.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void _cp(char *file_from, char *file_to);
+void _errexit(const char *format, const char *file, int code);
 
 #endif /* MAIN_H */
